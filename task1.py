e5b160a49ba7cd51741921737a691b171ed4d725
Niaz <yazzzik@list.ru>
2023-02-18 16:25:32 +0300

    comments

--- task1.py
+++ task1.py
@@ -3,19 +3,23 @@ from bs4 import BeautifulSoup
 
 
 def run():
+    # адрес сайта с фильмами
     url = "https://www.kinoafisha.info/rating/movies/"
     response = requests.get(url)
 
     soup = BeautifulSoup(response.content, 'lxml')
 
+    # поиск нужного элемента с ссылками на фильмы
     movies = soup.find('div', {'class': 'ratings_list'}).find_all('div', {'class': 'movieList_item'})
     index = ''
 
     num = 0
     for movie in movies:
-        num += 1
+        # извеление url, который ведет на страницу фильма
         url = movie.find('a')['href']
+        num += 1
 
+        # скачивание страницы и сохранение
         movie_page = requests.get(url)
         with open('movies/' + str(num) + '.html', "w", encoding="utf-8") as f:
             f.write(movie_page.text)
@@ -24,6 +28,7 @@ def run():
 
         print(str(num) + '/100')
 
+    # сохранение списка ссылок в отдельный файл
     with open('index.txt', "w", encoding="utf-8") as f:
         f.write(index)
 
